6eb4117a58716640ad6c65936497fbd2bc6f9443
William Hwang <willhwang90@gmail.com>
2022-06-09 10:53:53 -0400

    commit tree c4df1f37c04f723dcdcf8181a02c3adafaccf723

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+
+    def bark
+        puts "Woof!"
+    end
+
+    fido = Dog.new
+    fido.bark
+
+    snoopy = Dog.new
+    snoopy.bark
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
